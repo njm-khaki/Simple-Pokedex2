5b0c42076f1d0f9f3d89ac0650f3b09cf22c25c4
njm-khaki <cornet19970121@gmail.com>
2025-02-18 13:03:47 +0900

    ポケモン情報の取得

--- simple_pokedex/src/App.tsx
+++ simple_pokedex/src/App.tsx
@@ -1,33 +1,154 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios, { HttpStatusCode } from 'axios';
+import { useState } from 'react';
+import './App.css';
+
+interface ApiResponse<T> {
+  status: number;
+  body?: T;
+}
+
+interface PokemonList {
+  count: number;
+  next?: string;
+  previous?: string;
+  results: Array<PokemonIndex>;
+}
+
+interface PokemonIndex {
+  name: string;
+  url: string;
+}
+
+interface Pokemon {
+  name: string;
+  height: number;
+  weight: number;
+  sprites: PokemonSprite;
+  types: Array<PokemonType>;
+  stats: Array<PokemonStat>;
+  abilities: Array<PokemonAbility>;
+};
+
+interface PokemonSprite {
+  back_default?: string;
+  back_female?: string;
+  back_shiny?: string;
+  back_shiny_female?: string;
+  front_default?: string;
+  front_female?: string;
+  front_shiny?: string;
+  front_shiny_female?: string;
+}
+
+interface PokemonType {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+};
+
+interface PokemonStat {
+  base_stat: number;
+  effort: number;
+  stat: {
+    name: string;
+    url: string;
+  };
+};
+
+interface PokemonAbility {
+  ability: {
+    name: string;
+    url: string;
+  };
+  is_hidden: boolean;
+  slot: number;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemonList, setPokemonList] = useState<PokemonList>({
+    count: 0,
+    results: [],
+  });
+
+  const [pokemon, setPokemon] = useState<Pokemon>({
+    name: '',
+    height: 0,
+    weight: 0,
+    sprites: {},
+    types: [],
+    stats: [],
+    abilities: [],
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Simple Pokedex2</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={async () => {
+          const response = await axios.get(`https://pokeapi.co/api/v2/pokemon`)
+            .then((response) => {
+              return {
+                status: response.status,
+                body: response.data as PokemonList,
+              } as ApiResponse<PokemonList>;
+            });
+          if (response.status != HttpStatusCode.Ok || response.body == null) {
+            return;
+          }
+          console.log(response.body);
+          setPokemonList(response.body);
+        }}>
+          GET Pokemon LIST
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          count: {pokemonList.count}
         </p>
+        <p>
+          next: {pokemonList.next}
+        </p>
+        <p>
+          previous: {pokemonList.previous}
+        </p>
+        <button onClick={async () => {
+          const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/1`)
+            .then((response) => {
+              return {
+                status: response.status,
+                body: response.data,
+              } as ApiResponse<Pokemon>
+            });
+
+          if (response.body == null || response.status != HttpStatusCode.Ok) {
+            return;
+          }
+
+          console.log(response.body);
+          setPokemon(response.body);
+        }}>
+          GET Pokemon No. 1
+        </button>
+        <p>name: {pokemon.name}</p>
+        {
+          pokemon.sprites.front_default != null &&
+          <img src={pokemon.sprites.front_default} />
+        }
+        <p>
+          height: {pokemon.height}
+        </p>
+        <p>
+          weight: {pokemon.weight}
+        </p>
+        <p>
+          abilities
+        </p>
+        {
+          pokemon.abilities.map(
+            (ability) => <p>{ability.ability.name}</p>
+          )
+        }
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
