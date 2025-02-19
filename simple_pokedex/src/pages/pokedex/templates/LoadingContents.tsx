75aeac822816ea2c72f02cc7d8fccf804e38846f
njm-khaki <cornet19970121@gmail.com>
2025-02-19 12:20:51 +0900

    ポケモン詳細画面 UI実装

--- simple_pokedex/src/pages/pokedex/templates/LoadingContents.tsx
+++ simple_pokedex/src/pages/pokedex/templates/LoadingContents.tsx
@@ -3,16 +3,11 @@ import { Box, CircularProgress, Stack, Typography } from '@mui/material';
 const LoadingContents = () => {
   return (
     <Box display="flex" flexGrow={1} width="100%" height="90%">
-      <Stack justifyContent="center" alignItems="center">
+      <Stack justifyContent="center" alignItems="center" margin="0 auto">
+        <CircularProgress />
         <Typography>now loading...</Typography>
       </Stack>
     </Box>
-    // <Box display="flex" flexGrow={1} width="100%" height="90%">
-    //   <Stack justifyContent="center" alignItems="center" margin="0 auto">
-    //     <CircularProgress />
-    //     <Typography>now loading...</Typography>
-    //   </Stack>
-    // </Box>
   );
 };
 
--- simple_pokedex/src/pages/pokemon_detail/templates/LoadedContents.tsx
+++ simple_pokedex/src/pages/pokemon_detail/templates/LoadedContents.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material';
+import { Divider, Stack, Typography } from '@mui/material';
 
 const LoadedContents = () => {
   return (
@@ -6,7 +6,46 @@ const LoadedContents = () => {
       <img
         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png`}
       />
-      <Typography>ability</Typography>
+      <Divider />
+      <Stack direction="column">
+        <Typography>types</Typography>
+        <Stack direction="row" justifyContent="space-around">
+          <Typography>grass</Typography>
+          <Typography>fire</Typography>
+        </Stack>
+        <Divider />
+      </Stack>
+      <Stack direction="column">
+        <Typography>physical</Typography>
+        <Stack direction="row" justifyContent="space-around">
+          <Typography>height: 15</Typography>
+          <Typography>weight: 30</Typography>
+        </Stack>
+        <Divider />
+      </Stack>
+      <Stack direction="column">
+        <Typography>abilities</Typography>
+        <Stack direction="row" justifyContent="space-around">
+          <Typography>ability 1</Typography>
+          <Typography>ability 2</Typography>
+          <Typography>ability 3</Typography>
+        </Stack>
+        <Divider />
+      </Stack>
+      <Stack direction="column">
+        <Typography>stats</Typography>
+        <Stack direction="row" justifyContent="space-around">
+          <Typography>hp 80</Typography>
+          <Typography>atack 80</Typography>
+          <Typography>special atack 80</Typography>
+        </Stack>
+        <Stack direction="row" justifyContent="space-around">
+          <Typography>defence 80</Typography>
+          <Typography>special defence 80</Typography>
+          <Typography>speed 80</Typography>
+        </Stack>
+        <Divider />
+      </Stack>
     </>
   );
 };
